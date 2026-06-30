faad8b6ed755ff2006e71b54faf61a7609b3109e
olakojotobi89 <olakojotobi89@gmail.com>
2026-06-30 14:20:24 +0100

    Latest updates

---
+++ Cdre-e book-webapp/sw.js
@@ -0,0 +1,48 @@
+const cacheName = 'officer-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', async e => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+  return self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', async e => {
+  const req = e.request;
+  const url = new URL(req.url);
+
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkAndCache(req));
+  }
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName);
+  const cached = await cache.match(req);
+  return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+  const cache = await caches.open(cacheName);
+  try {
+    const refresh = await fetch(req);
+    await cache.put(req, refresh.clone());
+    return refresh;
+  } catch (e) {
+    return cache.match(req);
+  }
+}
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js').catch(err => console.log("SW registration failed", err));
+}
